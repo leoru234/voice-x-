a6c5c018370acd7712d2810ece24f95d79a40d3d
leoru234 <abhianshu120@gmail.com>
2026-05-26 12:28:16 +0530

    Create SplashActivity.kt

---
+++ app/src/main/java/com/voicex/app/ui/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.voicex.app.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { true }
+        lifecycleScope.launch {
+            delay(1500)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
